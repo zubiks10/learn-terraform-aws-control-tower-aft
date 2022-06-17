07cf28d62cb8d503c9e4321825f788ea0477f1c4
zubiks10 <zzubiks@gmail.com>
2022-06-17 16:56:05 +0100

    changes

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
   aft_management_account_id   = var.aft_management_account_id
   ct_home_region              = var.ct_home_region
-  tf_backend_secondary_region = var.tf_backend_secondary_region
+  tf_backend_secondary_region = eu-west-2
 
   vcs_provider                                  = "github"
   account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
